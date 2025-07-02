f1b0d3734cd09c6db7da5238805f30da02eb995b
Samuel Greig <samuel.greig@cis.strath.ac.uk>
2025-07-02 16:02:34 +0100

    Testing Env Variables

--- index.ts
+++ index.ts
@@ -1,15 +1,8 @@
 const express = require('express');
-// const app = express();
-// const PORT = 4000;
-// api/home.js
-module.exports = (req, res) => {
+const app = express();
+app.get('/', (req, res) => {
     const userId = process.env.EMAILJS_USER_ID;
     res.status(200).json(`Welcome, your app is working well. This is a test: ${userId}`);
-};
+});
 
-
-// app.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}`);
-// });
-
-// module.exports = app;
+module.exports = app;
